a3f66c7ef1fe62120ae75adbb62e9a7f919e139f
John Paul Fababaer <100891759+jpfababaer@users.noreply.github.com>
2024-04-05 09:28:52 +0000

    added booking form with calendar + adjusted navbar content

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,6 @@
 class BookingsController < ApplicationController
   before_action :set_booking, only: %i[ show edit update destroy ]
+  before_action :trainer_slots, only: %i[ new ]
 
   # GET /bookings or /bookings.json
   def index
@@ -23,9 +24,11 @@ class BookingsController < ApplicationController
   def create
     @booking = Booking.new(booking_params)
 
+    @booking.ended_at = @booking.started_at + 1.hour
+
     respond_to do |format|
       if @booking.save
-        format.html { redirect_to booking_url(@booking), notice: "Booking was successfully created." }
+        format.html { redirect_to bookings_path, notice: "Booking was successfully created." }
         format.json { render :show, status: :created, location: @booking }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -58,13 +61,36 @@ class BookingsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_booking
-      @booking = Booking.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def booking_params
-      params.require(:booking).permit(:started_at, :ended_at, :client_id, :trainer_id)
+  # Use callbacks to share common setup or constraints between actions.
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def booking_params
+    params.require(:booking).permit(:client_id, :trainer_id, :started_at, :ended_at)
+  end
+
+  def trainer_slots
+    @date = DateTime.parse(params[:date])
+    trainer_ids = Schedule.where(day_of_week: @date.strftime("%A")).pluck(:trainer_id).uniq
+    @trainers_array = User.where(id: trainer_ids)
+
+    @trainers_array.each do |trainer|
+      schedule = Schedule.where(day_of_week: @date.strftime("%A"))[0]
+      start_time = DateTime.new(@date.year, @date.month, @date.day, schedule.start_time.hour, schedule.start_time.min, schedule.start_time.sec)
+      end_time = DateTime.new(@date.year, @date.month, @date.day, schedule.end_time.hour, schedule.end_time.min, schedule.end_time.sec)
+
+      @hourly_slots = [] #Throws an error when User selects a date with no trainer availabilities.
+
+      current_time = start_time
+
+      while current_time < end_time
+        next_hour_time = current_time + 1.hour
+        @hourly_slots << { start_time: current_time, end_time: next_hour_time }
+        current_time = next_hour_time
+      end
     end
+  end
 end
