82971329335dd99798d08bcd900ce70c75ea5059
John Paul Fababaer <100891759+jpfababaer@users.noreply.github.com>
2024-04-22 15:38:25 +0000

    adjusted route after log-in

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-
   root "homes#landing"
-
   resources :bookings
   resources :schedules
-  devise_for :users
-
+  devise_for :users, controllers: { sessions: 'sessions' }
 end
